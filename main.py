ba55768505081f45a94dca41f5eb1385d1b264e6
Carapcula <ykozicin@gmail.com>
2024-03-16 17:25:23 +0300

    пончик

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return '<h1>Hello, World!</h1>'
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/main")
 def maining():
     return '<h1>Hello, Worssssld!</h1>'
+
+    
 app.run(debug=True)
